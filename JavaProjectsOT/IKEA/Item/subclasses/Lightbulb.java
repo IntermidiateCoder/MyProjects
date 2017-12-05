c9d55800bcae51d58daa23395158e87cfe22b5ca
hayun <hayun@DESKTOP-AH4PEVM>
2017-12-05 16:29:12 +0200

    +bedroom +lightbulb

---
+++ JavaProjectsOT/IKEA/Item/subclasses/Lightbulb.java
@@ -0,0 +1,9 @@
+package IKEA.Item.subclasses;
+
+public class Lightbulb extends BedRoomItem{
+
+	public Lightbulb(int itemID, String name, char sectionID) {
+		super(itemID, "Ligthbulb", 10, 2, sectionID);
+	}
+
+}
