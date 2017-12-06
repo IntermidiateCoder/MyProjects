c3dfcb6fd1fd901e360972f5e3af3a28700fdd37
hayun <hayun@DESKTOP-AH4PEVM>
2017-12-06 19:56:09 +0200

    .

--- JavaProjectsOT/IKEA/Item/subclasses/BedRoomItems/BedRoomItem.java
+++ JavaProjectsOT/IKEA/Item/subclasses/BedRoomItems/BedRoomItem.java
@@ -5,5 +5,5 @@ import IKEA.Item.Item;
 public class BedRoomItem extends Item {
 	public BedRoomItem(int itemID, String name, double sellPrice, double buyPrice) {
 		super(itemID, name, sellPrice, buyPrice, 'B');
-	}
+	} 
 }
